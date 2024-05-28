c302859cd894b021f83cbcbe4797581046d8141c
Harsh Kailash <harshkailash2000@gmail.com>
2024-05-29 00:14:41 +0530

    React Router

---
+++ React/06reactRouter/src/main.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/ContactUs/ContactUs";
+import User from "./components/User/User";
+import Github, { githubInfoLoader } from "./components/Github/Github";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contactUs",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contactUs" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} loader={githubInfoLoader} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
